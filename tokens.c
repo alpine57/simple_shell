cc6a8daa4d0f51ec5f043c7766a4a5d5af6ccb42
alpine57 <kamutileeongmail.com>
2023-12-15 14:31:35 -0800

    call

--- tokens.c
+++ tokens.c
@@ -1,48 +1,47 @@
 #include "main.h"
 
 char *fun_getline(void) {
-    static char buffer[BUFFER_SIZE];
-    static int buffer_pos = 0;
-    static int buffer_size = 0;
-
     char *line = NULL;
-    int line_size = 0;
-    char c;
+    ssize_t line_size = 0;
+    ssize_t buffer_size = BUFFER_SIZE;
+    ssize_t index = 0;
+    int c;
+
+    line = (char *)malloc(buffer_size * sizeof(char));
+    if (line == NULL) {
+        perror("Error: Memory allocation failed.\n");
+        exit(EXIT_FAILURE);
+    }
 
     while (1) {
-        if (buffer_pos >= buffer_size) {
-            buffer_size = read(STDIN_FILENO, buffer, BUFFER_SIZE);
-            buffer_pos = 0;
+        c = getchar();
 
-            if (buffer_size <= 0) {
-                if (line_size == 0) {
-                    return NULL;
-                } else {
-                    break;
-                }
-            }
-        }
-
-        c = buffer[buffer_pos++];
-
-        if (c == '\n' || c == '\0') {
+        if (c == EOF || c == '\n') {
+            line[index] = '\0';
             break;
         }
 
-        if (line == NULL) {
-            line = (char *)malloc(BUFFER_SIZE * sizeof(char));
-            if (line == NULL) {
-                perror("Error: Memory allocation failed.\n");
+        line[index++] = (char)c;
+
+       
+        if (index >= buffer_size - 1) {
+            buffer_size += BUFFER_SIZE;
+            char *temp = realloc(line, buffer_size * sizeof(char));
+            if (temp == NULL) {
+                perror("Error: Memory reallocation failed.\n");
+                free(line);
                 exit(EXIT_FAILURE);
             }
+            line = temp;
         }
-
-        line[line_size++] = c;
     }
 
-    if (line_size > 0) {
-        line[line_size] = '\0';
+   
+    if (line_size == 0 && c == EOF) {
+        free(line);
+        return NULL;
     }
 
     return line;
 }
+
