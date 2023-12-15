62f93df1303672a13efdd1436e56e586d296e62f
alpine57 <kamutileeongmail.com>
2023-12-15 13:17:09 -0800

    call

--- tokens.c
+++ tokens.c
@@ -1,50 +1,48 @@
 #include "main.h"
 
-char l;
-static char buffer[BUFFER_SIZE];
-static size_t buffer_pos = 0;
-static size_t buffer_size = 0;
+char *fun_getline(void) {
+    static char buffer[BUFFER_SIZE];
+    static int buffer_pos = 0;
+    static int buffer_size = 0;
 
-char *fun_getline() {
     char *line = NULL;
-    size_t line_size = 0;
-    size_t line_index = 0;
+    int line_size = 0;
+    char c;
 
     while (1) {
         if (buffer_pos >= buffer_size) {
-            buffer_size = fread(buffer, 1, BUFFER_SIZE, stdin);
-            if (buffer_size == 0) {
-                if (line_index == 0) {
+            buffer_size = read(STDIN_FILENO, buffer, BUFFER_SIZE);
+            buffer_pos = 0;
+
+            if (buffer_size <= 0) {
+                if (line_size == 0) {
                     return NULL;
                 } else {
                     break;
                 }
             }
-            buffer_pos = 0;
         }
 
-        l = buffer[buffer_pos++];
+        c = buffer[buffer_pos++];
 
-        if (l == '\n') {
+        if (c == '\n' || c == '\0') {
             break;
         }
 
-        if (line_index == line_size) {
-            line_size += BUFFER_SIZE;
-            line = realloc(line, line_size);
-            if (!line) {
-                perror("Memory allocation error");
+        if (line == NULL) {
+            line = (char *)malloc(BUFFER_SIZE * sizeof(char));
+            if (line == NULL) {
+                perror("Error: Memory allocation failed.\n");
                 exit(EXIT_FAILURE);
             }
         }
 
-        line[line_index++] = l;
+        line[line_size++] = c;
     }
 
-    if (line_index == 0) {
-        return NULL;
+    if (line_size > 0) {
+        line[line_size] = '\0';
     }
 
-    line[line_index] = '\0';
     return line;
 }
