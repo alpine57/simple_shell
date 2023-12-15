6b89b4996513198e273a06c70832aeda79cba046
alpine57 <kamutileeongmail.com>
2023-12-15 01:51:44 -0800

    simple

--- xitbuiltin.c
+++ xitbuiltin.c
@@ -1,17 +1,23 @@
 #include "main.h"
-
 void exit_shell(char **receive_argv, int execution_status) {
-    if (receive_argv[1] != NULL) {
-        int arg = atoi(receive_argv[1]);
-        if (arg >= 0) {
-            free(receive_argv);
-            exit(arg);
+    if (receive_argv && receive_argv[1] != NULL) {
+        char *arg_str = receive_argv[1];
+        char *endptr;
+        long arg = strtol(arg_str, &endptr, 10);
+
+        if (endptr != arg_str && *endptr == '\0') {
+            if (arg >= 0) {
+                free(receive_argv);
+                exit((int)arg);
+            } else {
+                fprintf(stderr, "Error: Illegal number: %ld\n", arg);
+            }
         } else {
-            fprintf(stderr, "Error: Illegal number: %s\n", receive_argv[1]);
+            fprintf(stderr, "Error: Invalid number format: %s\n", arg_str);
         }
     } else {
-        free(receive_argv);
+        if (receive_argv)
+            free(receive_argv);
         exit(execution_status);
     }
 }
-
