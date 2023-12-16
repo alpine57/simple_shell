a3d56c16c25cfd39e0a23e3a4554d2ab6cc38158
alpine57 <kamutileeongmail.com>
2023-12-16 06:49:02 -0800

    finally

--- commands.c
+++ commands.c
@@ -25,4 +25,51 @@ void execute_command(char **args) {
         } while (!WIFEXITED(status) && !WIFSIGNALED(status));
     }
 }
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+void cd_command(char **args) {
+    char *new_directory;
+    char *current_directory;
+    int chdir_status, setenv_status;
+
+    if (args[1] == NULL) {
+        new_directory = getenv("HOME");
+    } else if (strcmp(args[1], "-") == 0) {
+        new_directory = getenv("OLDPWD");
+    } else {
+        new_directory = args[1];
+    }
+
+    current_directory = getcwd(NULL, 0);
+
+    if (current_directory == NULL) {
+        perror("Error");
+        return;
+    }
+
+    chdir_status = chdir(new_directory);
+
+    if (chdir_status != 0) {
+        perror("Error");
+        free(current_directory);
+        return;
+    }
+
+    setenv_status = setenv("OLDPWD", current_directory, 1);
+
+    if (setenv_status != 0) {
+        perror("Error");
+    }
+
+    setenv_status = setenv("PWD", new_directory, 1);
+
+    if (setenv_status != 0) {
+        perror("Error");
+    }
+
+    free(current_directory);
+}
 
