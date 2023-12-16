c9e24375aa84b4135fc746394479709b7c7bfa2e
alpine57 <kamutileeongmail.com>
2023-12-16 03:09:22 -0800

    hell

--- prompt.c
+++ prompt.c
@@ -1,27 +1,28 @@
 #include "main.h"
-   
-   char *prompt_read(void) {
-       char *user_input = NULL;
-       ssize_t fd_check = -1;
-   
-       while (1) {
-           if (isatty(STDIN_FILENO) && write(STDOUT_FILENO, "$ ", 2) == -1) {
-               perror("write");
-              exit(EXIT_FAILURE);
-          }
-  
-          user_input = fun_getline();
-  
-          if (user_input == NULL) {
-              fprintf(stderr, "Error Unable to read input.\n");
-              continue;
-          }
-  
-          if (fd_check > 0) {
-              break;
-          }
-      }
-  
-  
-      return user_input;
-  }
+char *prompt_read(void) {
+    char *user_input = NULL;
+
+    while (1) {
+        if (isatty(STDIN_FILENO) && write(STDOUT_FILENO, "$ ", 2) == -1) {
+            perror("write");
+            exit(EXIT_FAILURE);
+        }
+
+        user_input = fun_getline();
+
+        if (user_input == NULL) {
+            if (isatty(STDIN_FILENO)) {
+                fprintf(stderr, "\n");
+                exit(EXIT_SUCCESS);
+            } else {
+                fprintf(stderr, "Error Unable to read input.\n");
+                continue;
+            }
+        }
+
+        break;
+    }
+
+    return user_input;
+}
+
